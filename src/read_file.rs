2422c5f5ebb01c3155dae7ee33822549f1ada8ee
Lorenzo Modica <lorenzomod@hotmail.it>
2025-02-24 18:30:42 +0100

    Empty File Check

--- src/read_file.rs
+++ src/read_file.rs
@@ -3,7 +3,13 @@ use std::io::{self, BufRead};
 use crate::GtexSummary;
 
 
-pub fn read_file<R: BufRead>(input: R) -> io::Result<GtexSummary> {
+pub fn read_file<R: BufRead>(mut input: R) -> io::Result<GtexSummary> {
+    
+    if input.fill_buf()?.is_empty() {
+        eprintln!("Warning: The file is empty.");
+        return Ok(GtexSummary::new());
+    }
+
     GtexSummary::from_reader(input, None)
 }
 
