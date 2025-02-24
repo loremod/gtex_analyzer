79a79904cb44216d2a1960961879bc4d1044891f
Lorenzo Modica <lorenzomod@hotmail.it>
2025-02-24 18:29:01 +0100

    Integration Tests

--- tests/test_read_file.rs
+++ tests/test_read_file.rs
@@ -1,4 +1,18 @@
+use gtex_analyzer::read_file;
+use std::fs::File;
+use std::io::{self, BufReader};
+
 #[test]
-fn test_load() {
-    
+fn test_load() -> io::Result<()> {
+    let file_path: &str = "data/GTEx_RNASeq_gene_median_tpm_HEAD.gct"; 
+
+    let file = File::open(file_path)?;
+    let reader = BufReader::new(file);
+
+    let summary = read_file(reader)?;
+
+    assert!(summary.metadata.is_some(), "Metadata should be present");
+    assert!(!summary.results.get_results().is_empty(), "Results should not be empty");
+
+    Ok(())
 }
